bcc5fa543160fa62337c743c24df8b01de1f1161
Andres Guisado <andresguisado@users.noreply.github.com>
2024-05-28 09:33:48 +0200

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,24 @@
+resource "azurerm_network_security_group" "aqua_agentless_scanner" {
+  for_each            = toset(var.aqua_volscan_scan_locations)
+  name                = "${var.aqua_network_security_group_name}-${each.value}"
+  location            = each.value
+  resource_group_name = var.aqua_volscan_resource_group_name
+  tags                = var.tags
+}
+
+resource "azurerm_virtual_network" "aqua_agentless_scanner" {
+  count               = length(var.aqua_volscan_scan_locations)
+  name                = "${var.aqua_virtual_network_name}-${var.aqua_volscan_scan_locations[count.index]}"
+  address_space       = local.virtual_network_address_space
+  location            = var.aqua_volscan_scan_locations[count.index]
+  resource_group_name = var.aqua_volscan_resource_group_name
+  tags                = var.tags
+}
+
+resource "azurerm_subnet" "aqua_agentless_scanner" {
+  count                = length(var.aqua_volscan_scan_locations)
+  name                 = var.aqua_subnet_name
+  resource_group_name  = var.aqua_volscan_resource_group_name
+  virtual_network_name = azurerm_virtual_network.aqua_agentless_scanner[count.index].name
+  address_prefixes     = local.subnet_address_prefixes
+}
